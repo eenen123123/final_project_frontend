18b3071e3c390235680d12e646f5affd41fa2d0f
kenzoair <diek056@naver.com>
2026-05-27 16:33:50 +0900

    QnA UI 수정

---
+++ src/types/board/QnaInterface.ts
@@ -0,0 +1,11 @@
+ export interface QnaItem {
+  postSn: number;
+  qnaCtgCd: string;
+  qnaCtgNm: string;
+  secrYn: string;
+  answStatCd: string;
+  answStatNm: string;
+  postSj: string;
+  wrtrUserId: string;
+  regDt: string;
+}
