101fbcd5671bbfbaddc2d7eb5ed872dc42568afd
kenzoair <diek056@naver.com>
2026-06-29 16:10:49 +0900

    공지사항 헤더 변경

--- src/pages/ServiceCenter/Notice/components/NoticeHeader.tsx
+++ src/pages/ServiceCenter/Notice/components/NoticeHeader.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function NoticeHeader() {
   return (
@@ -8,12 +8,24 @@ export default function NoticeHeader() {
           <h1 className="text-2xl font-extrabold text-gray-900 leading-tight">
             공지사항
           </h1>
-          <span className="text-xs text-gray-400">1:1 고객센터 공지 사항을 알려드립니다.</span>
+          <span className="text-xs text-gray-400">
+            고객센터 공지 사항을 알려드립니다.
+          </span>
         </div>
         <div className="text-xs text-gray-400 flex items-center gap-1">
-          <Link to="/customer" className="hover:text-blue-500 transition-colors">고객센터</Link>
+          <Link
+            to="/customer"
+            className="hover:text-blue-500 transition-colors"
+          >
+            고객센터
+          </Link>
           <span>&gt;</span>
-          <Link to="/customer/notice" className="hover:text-blue-500 transition-colors">공지사항</Link>
+          <Link
+            to="/customer/notice"
+            className="hover:text-blue-500 transition-colors"
+          >
+            공지사항
+          </Link>
         </div>
       </div>
       <hr className="border-t-2 border-gray-800 mt-2" />
