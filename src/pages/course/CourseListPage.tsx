db6c097d1dba261618056a21500a2ace7ad9cb84
eentks82137-dot <eentks82137@gmail.com>
2026-07-01 09:07:36 +0900

    fix: 강좌 목록 뷰 이미지 대신 강좌 정보로 필드 채움

--- src/pages/course/CourseListPage.tsx
+++ src/pages/course/CourseListPage.tsx
@@ -110,30 +110,45 @@ function CourseItem({ course }: { course: Course }) {
     <div className="group bg-white border border-gray-100 rounded-2xl shadow-sm flex items-start gap-3 sm:gap-5 py-4 px-4 sm:py-5 sm:px-5 hover:shadow-md transition-all duration-200">
       {/* 썸네일 */}
       <div className="shrink-0">
-        {course.thumbnailImg ? (
+        {/* {course.thumbnailImg ? (
           <img
             src={course.thumbnailImg}
             alt={course.courseName}
             className="w-16 h-24 sm:w-32 sm:h-44 object-cover rounded-xl shadow-sm"
           />
-        ) : (
-          <div className="w-16 h-24 sm:w-32 sm:h-44 bg-blue-400 rounded-xl shadow-sm p-2 sm:p-3 flex flex-col justify-between text-white border-l-4 border-black/20">
-            <div className="space-y-0.5 sm:space-y-1">
-              <span className="text-[8px] sm:text-[11px] bg-white/20 px-1 py-0.5 rounded font-bold tracking-widest uppercase block">
-                HERMES
-              </span>
-              <h5 className="text-[9px] sm:text-[12px] font-bold leading-tight line-clamp-2 mt-1">
-                {course.subjectName}
-              </h5>
-              <h5 className="text-[10px] sm:text-[13px] font-bold leading-tight line-clamp-2 mt-1">
-                {course.courseName}
-              </h5>
-            </div>
-            <span className="text-[8px] sm:text-[11px] font-medium text-right block opacity-80">
-              {course.instructorName}
+        ) : ( */}
+        <div
+          className={`w-16 h-24 sm:w-32 sm:h-44
+        
+        ${
+          course.courseName.includes("수능특강")
+            ? "bg-linear-to-r from-blue-400 to-indigo-400"
+            : course.courseName.includes("수능완성")
+              ? "bg-linear-to-r from-yellow-400 to-amber-400"
+              : "bg-red-400/80"
+        }
+         
+         rounded-xl shadow-sm p-2 sm:p-3 flex flex-col justify-between text-white border-l-4 border-black/20`}
+        >
+          <div className="space-y-0.5 sm:space-y-1">
+            <span className="text-[8px] sm:text-[11px] bg-white/20 px-1 py-0.5 rounded font-bold tracking-widest uppercase block">
+              HERMES
             </span>
+            <h5 className="text-[8px] sm:text-[12px] font-bold leading-tight line-clamp-2 mt-1 hidden sm:block">
+              {course.subjectName}
+            </h5>
+            <h5 className="text-[9px] sm:text-[13px] font-bold leading-tight line-clamp-2 mt-1">
+              {course.courseName.split("]")[0] + "]"}
+            </h5>
+            <h5 className="text-[9px] sm:text-[13px] font-bold leading-tight line-clamp-2 mt-1">
+              {course.courseName.split("]")[1]?.trim()}
+            </h5>
           </div>
-        )}
+          <span className="text-[8px] sm:text-[11px] font-medium text-right block opacity-80">
+            {course.instructorName}
+          </span>
+        </div>
+        {/* )} */}
       </div>
 
       {/* 콘텐츠 */}
